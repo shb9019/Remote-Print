f7c5e391299eaf06232eab713d5ced61ac402d3b
shb9019 <saihemanth9019@gmail.com>
2017-10-26 10:43:47 +0530

    Added Sessions

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class LoginController extends Controller
 {
-    //
+	public function login(Request $request)
+	{
+		if($request->session()->has('users'))
+		{
+			return view('');
+		}	
+
+	}
+
 }
